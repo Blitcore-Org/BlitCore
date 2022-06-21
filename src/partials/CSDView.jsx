d75e4606d74f0605b50ccf20234f8371b558d810
tamer <tamerjar@hotmail.com>
2022-06-21 17:22:22 +0300

    trying new layout and fixed allignment of the svg

--- src/partials/CSDView.jsx
+++ src/partials/CSDView.jsx
@@ -12,28 +12,28 @@ import csdtop from "../images/CSD/csd12.png";
 
 function CSDView() {
   return (
-    <section class="w-full bg-white pt-7 pb-7 md:pt-20 md:pb-24">
+    <section class="w-full bg-blitblue-100 pt-7 pb-7 md:pt-20 md:pb-24">
     <div class="box-border flex flex-col items-center content-center px-8 mx-auto leading-6 text-black border-0 border-gray-300 border-solid md:flex-row max-w-7xl lg:px-16">
 
         <div class="box-border relative w-full max-w-md px-4 mt-5 mb-4 -ml-5 text-center bg-no-repeat bg-contain border-solid md:ml-0 md:mt-0 md:max-w-none lg:mb-0 md:w-1/2 xl:pl-10">
             <img src="https://cdn.devdojo.com/images/december2020/productivity.png" class="p-2 pl-6 pr-5 xl:pl-16 xl:pr-20 "/>
         </div>
 
-        <div class="box-border order-first w-full text-black border-solid md:w-1/2 md:pl-10 md:order-none">
-            <h2 class="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl">
+        <div class="box-border order-first w-full text-blitblue-500 border-solid md:w-1/2 md:pl-10 md:order-none">
+            <h2 class="m-0 text-xl font-semibold leading-tight border-0 border-blittext-100 0 lg:text-3xl md:text-2xl">
                 Customized Apps
             </h2>
-            <p class="pt-4 pb-8 m-0 leading-7 text-gray-700 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg">
+            <p class="pt-4 pb-8 m-0 leading-7 text-blitblue-500 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg">
                 Build an atmosphere that creates productivity in your organization and your company culture.
             </p>
             <ul class="p-0 m-0 leading-6 border-0 border-gray-300">
-                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                <li class="box-border relative py-1 pl-0 text-left text-blittext-100 border-solid">
                     <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full"><span class="text-sm font-bold">✓</span></span> Custom Software Design and Development
                 </li>
-                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                <li class="box-border relative py-1 pl-0 text-left text-blittext-100 border-solid">
                     <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full"><span class="text-sm font-bold">✓</span></span> Free UI/UX Plan of the Software
                 </li>
-                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                <li class="box-border relative py-1 pl-0 text-left text-blittext-100 border-solid">
                     <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full"><span class="text-sm font-bold">✓</span></span> Programming Language of Choice
                 </li>
             </ul>
@@ -41,21 +41,21 @@ function CSDView() {
     </div>
     <div class="box-border flex flex-col items-center content-center px-8 mx-auto mt-2 leading-6 text-black border-0 border-gray-300 border-solid md:mt-20 xl:mt-0 md:flex-row max-w-7xl lg:px-16">
 
-        <div class="box-border w-full text-black border-solid md:w-1/2 md:pl-6 xl:pl-32">
+        <div class="box-border w-full text-blitblue-400 border-solid md:w-1/2 md:pl-6 xl:pl-32">
             <h2 class="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl">
                 Seemless Design
             </h2>
-            <p class="pt-4 pb-8 m-0 leading-7 text-gray-700 border-0 border-gray-300 sm:pr-10 lg:text-lg">
+            <p class="pt-4 pb-8 m-0 leading-7 text-blitblue-500 border-0 border-gray-300 sm:pr-10 lg:text-lg">
                 Save time and money with our revolutionary services. We are the leaders in the industry.
             </p>
             <ul class="p-0 m-0 leading-6 border-0 border-gray-300">
-                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                <li class="box-border relative py-1 pl-0 text-left text-blittext-100 border-solid">
                     <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full"><span class="text-sm font-bold">✓</span></span> CRM Software Development
                 </li>
-                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                <li class="box-border relative py-1 pl-0 text-left text-blittext-100 border-solid">
                     <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full"><span class="text-sm font-bold">✓</span></span> Business System Integration
                 </li>
-                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                <li class="box-border relative py-1 pl-0 text-left text-blittext-100 border-solid">
                     <span class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full"><span class="text-sm font-bold">✓</span></span> Enterprise Mobility Solutions
                 </li>
             </ul>
@@ -65,25 +65,25 @@ function CSDView() {
             <img src="https://cdn.devdojo.com/images/december2020/settings.png" class="pl-4 sm:pr-10 xl:pl-10 lg:pr-32"/>
         </div>
     </div>
-    <section class="py-20 bg-gray-50">
+    <section class="py-20  bg-blitblue-100">
     <div class="container items-center max-w-6xl px-4 px-10 mx-auto sm:px-20 md:px-32 lg:px-16">
     <div class="flex flex-wrap items-center -mx-3">
       <div class="order-1 w-full px-3 lg:w-1/2 lg:order-0">
         <div class="w-full lg:max-w-md">
-          <h2 class="mb-4 text-3xl font-bold leading-tight tracking-tight sm:text-4xl font-heading">Jam-packed with all the services you need!</h2>
-          <p class="mb-4 font-medium tracking-tight text-gray-400 xl:mb-6">It's never been easier to have a Custom Software Idea of yours!</p>
+          <h2 class="mb-4 text-3xl text-blitblue-400 font-bold leading-tight tracking-tight sm:text-4xl font-heading">Jam-packed with all the services you need!</h2>
+          <p class="mb-4 font-medium tracking-tight text-blittext-100 xl:mb-6">It's never been easier to have a Custom Software Idea of yours!</p>
           <ul>
             <li class="flex items-center py-2 space-x-4 xl:py-3">
               <svg class="w-8 h-8 text-pink-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path></svg>
-              <span class="font-medium text-gray-500">Faster Processing and Delivery</span>
+              <span class="font-medium text-blittext-100">Faster Processing and Delivery</span>
             </li>
             <li class="flex items-center py-2 space-x-4 xl:py-3">
               <svg class="w-8 h-8 text-yellow-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path></svg>
-              <span class="font-medium text-gray-500">Out of the Box Tracking and Monitoring</span>
+              <span class="font-medium text-blittext-100">Out of the Box Tracking and Monitoring</span>
             </li>
             <li class="flex items-center py-2 space-x-4 xl:py-3">
               <svg class="w-8 h-8 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path></svg>
-              <span class="font-medium text-gray-500">100% Protection and Security for Your App</span>
+              <span class="font-medium text-blittext-100">100% Protection and Security for Your App</span>
             </li>
           </ul>
         </div>
