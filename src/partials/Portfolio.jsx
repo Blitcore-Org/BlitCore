c4c88abd61b2da60d0216741c9d894750f30eab0
abassj1 <abass-jam@hotmail.com>
2023-10-25 17:02:47 +0100

    Portfolio design prototype 5

--- src/partials/Portfolio.jsx
+++ src/partials/Portfolio.jsx
@@ -1,108 +1,120 @@
-import React, { useState } from 'react';
-import { HiChevronRight, HiChevronLeft } from 'react-icons/hi';
-import { FaWalking, FaBiking, FaCar, FaPlane } from 'react-icons/fa';
-import {BsWhatsapp} from 'react-icons/bs';
+import React, { useState, useEffect } from "react";
+import { HiChevronRight, HiChevronLeft } from "react-icons/hi";
+import {
+  BsWhatsapp,
+  BsFillHousesFill,
+  BsScissors,
+  BsPersonVideo,
+} from "react-icons/bs";
+import { HiChevronUp, HiChevronDown } from "react-icons/hi";
+
+const isMobile = window.innerWidth <= 768; // You can adjust the breakpoint if needed
 
 const CARDS = 10;
 const MAX_VISIBILITY = 3;
 
 const Card = ({ name, label, icon, backgroundImage }) => (
   <div className="car-card">
-    <div className="cover-image flex h-full w-full" style={{ backgroundImage }}></div>
-    <div className="icon-container mt-8 flex flex-row">
-      <div className="icon h-14 w-14">
+    <div
+      className="cover-image flex h-full w-full"
+      style={{ backgroundImage }}
+    ></div>
+    <div className="icon-container mt-5 md:mt-8 flex flex-row">
+      <div className="icon my-1 h-8 w-8 md:h-14 md:w-14 text-sm md:text-lg">
         {icon}
       </div>
-      <div className="info-containter">
-        <h2>{name}</h2>
-        <p>{label}</p>
+      <div className="info-containter md:my-2">
+        <h2 className="text-md font-bold md:text-lg">{name}</h2>
+        <p className="text-sm md:text-md">{label}</p>
       </div>
     </div>
   </div>
 );
 
-
 const Carousel = ({ children }) => {
   const [active, setActive] = useState(2);
   const count = React.Children.count(children);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div className="carousel">
-      {active > 0 && <button className='nav left' onClick={() => setActive(i => i - 1)}><HiChevronLeft /></button>}
+      {active > 0 && (
+        <button className="nav left" onClick={() => setActive((i) => i - 1)}>
+          {isMobile ? <HiChevronUp /> : <HiChevronLeft />}
+        </button>
+      )}
       {React.Children.map(children, (child, i) => (
-        <div className='car-card-container' style={{
-          '--active': i === active ? 1 : 0,
-          '--offset': (active - i) / 3,
-          '--direction': Math.sign(active - i),
-          '--abs-offset': Math.abs(active - i) / 2,
-          'pointerEvents': active === i ? 'auto' : 'none',
-          'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
-          'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block',
-        }}> 
+        <div
+          className="car-card-container"
+          style={{
+            "--active": i === active ? 1 : 0,
+            "--offset": (active - i) / 3,
+            "--direction": Math.sign(active - i),
+            "--abs-offset": Math.abs(active - i) / 2,
+            "--vertical-direction": isMobile ? "1" : "0",
+            pointerEvents: active === i ? "auto" : "none",
+            opacity: Math.abs(active - i) >= MAX_VISIBILITY ? "0" : "1",
+            display: Math.abs(active - i) > MAX_VISIBILITY ? "none" : "block",
+          }}
+        >
           {child}
         </div>
       ))}
-      {active < count - 1 && <button className='nav right' onClick={() => setActive(i => i + 1)}><HiChevronRight /></button>}
+      {active < count - 1 && (
+        <button className="nav right" onClick={() => setActive((i) => i + 1)}>
+          {isMobile ? <HiChevronDown /> : <HiChevronRight />}
+        </button>
+      )}
     </div>
   );
 };
 
 function Portfolio() {
-
   // Options data state.
-const [options] = useState([
-  {
-    name: "Option 1",
-    label: "Label 1",
-    icon: <BsWhatsapp />,
-    backgroundImage:
-      "url('src/images/Portfolio/WhatsappAutoDesign/cover.png')",
-  },
+  const [options] = useState([
+    {
+      name: "Whatsapp Automation",
+      label: "Application Design",
+      icon: <BsWhatsapp />,
+      backgroundImage:
+        "url('src/images/Portfolio/WhatsappAutoDesign/cover.png')",
+    },
 
-  {
-    name: "Option 2",
-    label: "Label 2",
-    icon: <BsWhatsapp />,
-    backgroundImage: "url('src/images/Portfolio/RiadMDesign/cover.png')",
-  },
+    {
+      name: "Marrakesh Rentals",
+      label: "Website Design",
+      icon: <BsFillHousesFill />,
+      backgroundImage: "url('src/images/Portfolio/RiadMDesign/cover.png')",
+    },
 
-  {
-    name: "Option 3",
-    label: "Label 3",
-    icon: <BsWhatsapp />,
-    backgroundImage:
-      "url('src/images/Portfolio/PersonalWebDesign/cover.png')",
-  },
+    {
+      name: "Personal Website",
+      label: "Web Development",
+      icon: <BsPersonVideo />,
+      backgroundImage:
+        "url('src/images/Portfolio/PersonalWebDesign/cover.png')",
+    },
 
-  {
-    name: "Option 4",
-    label: "Label 4",
-    icon: <BsWhatsapp />,
-    backgroundImage:
-      "url('src/images/Portfolio/ArthurRazorDesign/cover.png')",
-  },
-  {
-    name: "Option 5",
-    label: "Label 5",
-    icon: <BsWhatsapp />,
-    backgroundImage:
-      "url('src/images/Portfolio/ArthurRazorDesign/cover.png')",
-  },
-  {
-    name: "Option 6",
-    label: "Label 6",
-    icon: <BsWhatsapp />,
-    backgroundImage:
-      "url('src/images/Portfolio/ArthurRazorDesign/cover.png')",
-  },
-  {
-    name: "Option 7",
-    label: "Label 7",
-    icon: <BsWhatsapp />,
-    backgroundImage:
-      "url('src/images/Portfolio/ArthurRazorDesign/cover.png')",
-  },
-]);
+    {
+      name: "Arthur Razor",
+      label: "Website Design",
+      icon: <BsScissors />,
+      backgroundImage:
+        "url('src/images/Portfolio/ArthurRazorDesign/cover.png')",
+    },
+  ]);
 
   return (
     <section className="relative">
@@ -114,14 +126,14 @@ const [options] = useState([
                 Portfolio
               </span>
             </h1>
-            <div className="justify-center mt-10 flex flex-col md:flex-row">
+            <div className="justify-center mt-10 flex">
               <Carousel>
                 {options.map((option, i) => (
-                  <Card 
-                    key={i} 
-                    name={option.name} 
-                    label={option.label} 
-                    icon={option.icon} 
+                  <Card
+                    key={i}
+                    name={option.name}
+                    label={option.label}
+                    icon={option.icon}
                     backgroundImage={option.backgroundImage}
                   />
                 ))}
@@ -135,4 +147,3 @@ const [options] = useState([
 }
 
 export default Portfolio;
-
